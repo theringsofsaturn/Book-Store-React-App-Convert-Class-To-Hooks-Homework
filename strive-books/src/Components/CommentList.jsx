de1438103cf15f861f122ab39200f247dfa9b2b7
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-10-05 17:00:14 +0200

    CommentList converted

--- strive-books/src/Components/CommentList.jsx
+++ strive-books/src/Components/CommentList.jsx
@@ -1,11 +1,10 @@
 import ListGroup from "react-bootstrap/ListGroup";
 import SingleComment from "./SingleComment";
 
-const CommentList = ({ commentsProps }) => (
-  <ListGroup>
-    {console.log({ commentsProps })}
-    {commentsProps.map((element) => (
-      <SingleComment comment={element} key={element._id} />
+const CommentList = ({ commentsToShow }) => (
+  <ListGroup style={{ color: "black" }}>
+    {commentsToShow.map((comment) => (
+      <SingleComment comment={comment} key={comment._id} />
     ))}
   </ListGroup>
 );
