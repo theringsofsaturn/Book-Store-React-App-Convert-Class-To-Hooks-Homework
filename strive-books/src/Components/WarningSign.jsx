669c4199f982230eaff0e3db0895b71a847b9625
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-10-05 17:04:08 +0200

    WarningSign

--- strive-books/src/Components/WarningSign.jsx
+++ strive-books/src/Components/WarningSign.jsx
@@ -1,4 +1,5 @@
 import Alert from "react-bootstrap/Alert";
 
-const WarningSign = (props) => <Alert variant={props.danger}>{props.text}</Alert>;
+const WarningSign = (props) => <Alert variant="danger">{props.text}</Alert>;
+
 export default WarningSign;
